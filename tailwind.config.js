0ca127eca9c14c517a9fa8cfc5cff06e87854789
subhmsingha1921 <eagleofnile27@gmail.com>
2023-01-17 13:27:28 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
